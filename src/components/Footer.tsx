e3c7e02cb9f90bf7c312b00ac9fb100a027bf140
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-23 15:17:13 +0500

    created

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,20 +5,6 @@ import Link from "next/link";
 import React from "react";
 import { FaFacebook, FaSquareInstagram, FaLinkedin, FaWhatsapp, FaXTwitter } from "react-icons/fa6";
 const Footer = () => {
-    let contact_data = [
-        {
-            name: "Address:",
-            link: "3891 Ranchview Dr. Richardson, California 62639",
-        },
-        {
-            name: "Send Email:",
-            link: "Info@gmail.com",
-        },
-        {
-            name: "Call Emergency:",
-            link: "+88 (308) 555-0121",
-        },
-    ];
     const nav_data = [
         {
             link: '/',
@@ -49,6 +35,14 @@ const Footer = () => {
             name: 'Contact Us',
         },
 
+    ];
+
+    const services_data = [
+        'Managed Security Services',
+        'Network Security',
+        'Cloud Security',
+        'Data Protection',
+        'Compliance'
     ]
     return (
         <div className="w-full bg-secondary  h-auto gap-5 flex justify-center items-center py-10 px-3">
@@ -56,14 +50,14 @@ const Footer = () => {
                 <div className="footerContent max-sm:justify-center w-full h-auto grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 justify-between mb-3 gap-6 ">
                     <div className="left w-full max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center p-3 h-auto">
                         {/* Logo */}
-                        <div className="logo ">
-                            <Image src='/images/logo.png' alt="image" width={200} height={200} />
-                            <div className="flex gap-3 flex-wrap h-auto  p-3">
-                                <FaFacebook size={24} />
-                                <FaXTwitter size={24} />
-                                <FaSquareInstagram size={24} />
-                                <FaLinkedin size={24} />
-                                <FaWhatsapp size={24} />
+                        <div className="logo flex flex-col gap-10 ">
+                            <Image src='/images/footer-logo.png' alt="image" width={220} height={110} />
+                            <div className="flex gap-3 flex-wrap h-auto px-3">
+                                <Image src='/images/facebook.png' alt="image" width={30} height={30} />
+                                <Image src='/images/twitter.png' alt="image" width={30} height={30} />
+                                <Image src='/images/linkedin.png' alt="image" width={30} height={30} />
+                                <Image src='/images/instagram.png' alt="image" width={30} height={30} />
+                                <Image src='/images/whatsapp.png' alt="image" width={30} height={30} />
 
                             </div>
                         </div>
@@ -87,28 +81,27 @@ const Footer = () => {
                     <div className="middle  max-sm:py-3 w-full p-3 h-auto">
                         <h4 className='font-mono lg:text-2xl text-xl font-bold text-[#BA23D2] w-fit'>Our Services</h4>
                         <ul className="list-none py-2 gap-2 flex flex-col ">
-                            <li className="text-sm  font-normal ">
-                                Undergraduate Programs
-                            </li>
-                            <li className="text-sm  font-normal">
-                                Graduate Programs
-                            </li>
-                            <li className="text-sm  font-normal">
-                                Online Education
-                            </li>
+                            
+                             {
+                                services_data.map((services, index) => (
+                                    <li key={index} className="text-sm  font-normal">
+                                        {services}
+                                    </li>
+                                ))
+                            }
                         </ul>
                     </div>
                     <div className="left max-sm:w-full max-sm:px-3 gap-10  max-sm:py-3 w-full p-3  h-auto max-[900px]:w-full">
                         <h4 className='font-mono lg:text-2xl text-xl font-bold text-[#BA23D2] w-fit'> Contact Information</h4>
                         <ul className="list-none py-2 gap-2 flex flex-col ">
                             <li className="text-sm  font-normal ">
-                                [University Address]
+                               [Your Office Location]
                             </li>
                             <li className="text-sm  font-normal">
-                                [University Phone Number]
+                              [Your Phone Number]
                             </li>
                             <li className="text-sm  font-normal">
-                                admissions@[youruniversity].edu
+                               contact@[yourdomain].com
                             </li>
                         </ul>
                     </div>
