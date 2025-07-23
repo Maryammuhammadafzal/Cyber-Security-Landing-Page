c34bbd15d00d8d55b1166843cb76e7e28bb204c7
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-23 12:22:00 +0500

    adding data

--- src/app/testimonial/page.tsx
+++ src/app/testimonial/page.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
 
 const TestimonialPage = () => {
+    const testimonial_data = [
+        {
+            title: 'CTO, Financial Firm',
+            description: '"They detected vulnerabilities we didn’t even know existed. We feel much safer now."',
+        },
+        {
+            title: 'CEO, E-commerce Platform',
+            description: '"Their team handled our breach response quickly and professionally — saved us from major loss."',
+        },
+        {
+            title: 'IT Manager, Healthcare ',
+            description: '"We trust them completely with our cloud infrastructure security."',
+        },
+    ]
     return (
         <div className="about mb-0 sm:p-0 px-4 gap-8 sm:w-[90%] md:w-[99%] lg:w-[85%] w-full mx-auto flex-col h-auto flex justify-center  items-center xl:py-30 lg:py-24 md:py-18 sm:py-12 xs:py-7 py-5">
             <div className='flex w-full'>
                 <h2 className="font-semibold max-w-lg w-fit gradient-title xl:text-6xl lg:text-5xl md:text-5xl min-[768px]:text-4xl text-6xl xl:leading-16 lg:leading-14 max-sm:text-5xl max-xs:text-4xl">
-                   Testimonials
+                    Testimonials
                 </h2>
             </div>
             <div className="w-full p-2 h-auto flex justify-center items-center">
-                
+                {testimonial_data.map((testimonial , index)=> (
+                    <div></div>
+                ))
+
+                }
             </div>
         </div>
     )
