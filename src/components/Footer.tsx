18d76e374553c180e42f3964f678d967ef32d290
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-23 15:06:22 +0500

    creating Footer data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,6 +19,37 @@ const Footer = () => {
             link: "+88 (308) 555-0121",
         },
     ];
+    const nav_data = [
+        {
+            link: '/',
+            name: 'Home',
+        },
+        {
+            link: '/',
+            name: 'About Us',
+        },
+        {
+            link: '/',
+            name: 'Services',
+        },
+        {
+            link: '/',
+            name: 'Process',
+        },
+        {
+            link: '/',
+            name: 'Testimonials',
+        },
+        {
+            link: '/',
+            name: 'Pricing Plans',
+        },
+        {
+            link: '/',
+            name: 'Contact Us',
+        },
+
+    ]
     return (
         <div className="w-full bg-secondary  h-auto gap-5 flex justify-center items-center py-10 px-3">
             <div className=" w-[80%] max-lg:w-[95%] max-md:w-full  h-auto flex flex-col gap-10 justify-center items-center">
@@ -28,57 +59,33 @@ const Footer = () => {
                         <div className="logo ">
                             <Image src='/images/logo.png' alt="image" width={200} height={200} />
                             <div className="flex gap-3 flex-wrap h-auto  p-3">
-                            <FaFacebook size={24} />
-                            <FaXTwitter size={24} />
-                            <FaSquareInstagram size={24} />
-                            <FaLinkedin size={24} />
-                            <FaWhatsapp size={24} />
+                                <FaFacebook size={24} />
+                                <FaXTwitter size={24} />
+                                <FaSquareInstagram size={24} />
+                                <FaLinkedin size={24} />
+                                <FaWhatsapp size={24} />
 
+                            </div>
                         </div>
-                        </div>
-                        
+
                     </div>
 
                     <div className="max-sm:w-full max-sm:py-3 w-full p-3 h-auto ">
                         <h4 className='font-mono lg:text-2xl text-xl font-bold text-[#BA23D2] w-fit'>Quick Links</h4>
                         <ul className="list-none text-base py-2  gap-2 flex flex-col ">
-                            <li className="  font-normal">
-                                <a href="#" className="">
-                                    Home
-                                </a>
-                            </li>
-                            <li className="  font-normal">
-                                <a href="#" className="">
-                                    About Us
-                                </a>
-                            </li>
-                            <li className="  font-normal">
-                                <a href="#" className="">
-                                    Campus Life
-                                </a>
-                            </li>
-                            <li className="  font-normal">
-                                <a href="#" className="">
-                                    How To Apply
-                                </a>
-                            </li>
-                            <li className="  font-normal">
-                                <a href="#" className="">
-                                    Testimonial
-                                </a>
-                            </li>
-
-                            <li className="  font-normal">
-                                <a href="#" className="">
-                                    Contact us
-                                </a>
-                            </li>
+                            {
+                                nav_data.map((nav, index) => (
+                                    <li key={index} className="font-normal">
+                                        <Link href={nav.link} className="">
+                                            {nav.name}
+                                        </Link>
+                                    </li>
+                                ))
+                            }
                         </ul>
                     </div>
                     <div className="middle  max-sm:py-3 w-full p-3 h-auto">
-                        <h4 className=" text-xl capitalize font-semibold">
-                            Our Programs
-                        </h4>
+                        <h4 className='font-mono lg:text-2xl text-xl font-bold text-[#BA23D2] w-fit'>Our Services</h4>
                         <ul className="list-none py-2 gap-2 flex flex-col ">
                             <li className="text-sm  font-normal ">
                                 Undergraduate Programs
@@ -92,9 +99,7 @@ const Footer = () => {
                         </ul>
                     </div>
                     <div className="left max-sm:w-full max-sm:px-3 gap-10  max-sm:py-3 w-full p-3  h-auto max-[900px]:w-full">
-                        <h4 className=" text-lg capitalize font-semibold ">
-                            Contact Information
-                        </h4>
+                        <h4 className='font-mono lg:text-2xl text-xl font-bold text-[#BA23D2] w-fit'> Contact Information</h4>
                         <ul className="list-none py-2 gap-2 flex flex-col ">
                             <li className="text-sm  font-normal ">
                                 [University Address]
